a7a520e7c3377c5b2279601dbd60032e6f7fd3cb
Ronald Müller <ronald.mueller@gmail.com>
2013-10-09 15:00:43 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfoparkApp::Application.config.secret_token = 'e1df866593240ae0da2545ef7411fa707ed9fab549ae6827121107116b8ba1bec5cbb69c627d412c5c7a1697e98180d18f5ce8ed11e5b3aa29b9ae896562d900'
